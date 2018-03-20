fd6a56e85f17a7c5515f14e7b1355991abc68705
vaishnavig511 <37594834+vaishnavig511@users.noreply.github.com>
2018-03-21 04:12:49 +0530

    Header File

---
+++ Vehicles.hpp
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+using namespace cv;
+using namespace std;
+
+class Vehicles
+{
+        private:
+                Rect r;
+                Mat im;
+                double v;
+                double ts;                 //TimeStamp: Last update time
+
+        public:
+                Vehicles(Rect re, Mat i, double t) : r(re), im(i(re)), v(0), ts(t) {};
+                Vehicles& operator=(const Vehicles& c);
+
+                int area();
+                Point2f pos();
+                double velocity();
+
+                void plot(Mat& f, int);
+
+                bool onScene(Mat& f, double time);
+                bool match(Vehicles& c);     //Assume function is being called from the 'newest' car.
+
+};
+
