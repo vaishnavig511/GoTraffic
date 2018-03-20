670b99f99ca732995dc9bdfccc69cd434663f5d5
vaishnavig511 <37594834+vaishnavig511@users.noreply.github.com>
2018-03-21 04:05:00 +0530

    Add files via upload
    main file of the application

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "TrafficFlow.hpp"
+
+
+int main(int argc,char *argv[])
+{
+        TrafficFlow t;
+        string s;
+
+        system("Color A");
+        cout<<"\t\t******WELCOME TO TRAFFIC FLOW CONTROL SYSTEM******\t\t\n";
+
+
+        if (argc < 2)
+        {
+
+                cout << "Press any key to continue" << endl;
+                cin>>s;
+                 t.generateConfig(s);
+                 return 0;
+        }
+
+        return 0;
+}
